0d9c04971ad324bddd1832f6e416417c3839fbd0
uttam <uttam@192.168.1.4>
2023-12-10 18:04:38 +0545

    Nav bar including theme-switch and a dashboard layout has been implemented

---
+++ constants/theme.tsx
@@ -0,0 +1,16 @@
+import { Computer, Moon, Sun } from "lucide-react";
+
+export const themes = [
+  {
+    icon: Sun,
+    label: "Light",
+  },
+  {
+    icon: Moon,
+    label: "Dark",
+  },
+  {
+    icon: Computer,
+    label: "System",
+  },
+];
